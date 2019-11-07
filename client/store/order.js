b30147c3aecf06ffe2b69ebb5952872fb83e2ae5
Chia-Li Chen <cl3chen@gmail.com>
2019-11-07 17:20:51 -0500

    Starting front-end for updating cart

---
+++ client/store/order.js
@@ -0,0 +1,79 @@
+import axios from 'axios'
+
+/*
+ * ACTION TYPES
+ */
+const FETCH_PRODUCTS = 'FETCH_PRODUCTS'
+const FETCH_PRODUCT = 'FETCH_PRODUCT'
+const CREATE_CART = 'CREATE_CART'
+const ADD_TO_CART = 'ADD_TO_CART'
+
+/**
+ * INITIAL STATE
+ */
+const initialState = {
+  productList: [],
+  cart: {}
+}
+
+/**
+ * ACTION CREATORS
+ */
+const fetchProductsAction = products => ({type: FETCH_PRODUCTS, products})
+const fetchProductAction = product => ({type: FETCH_PRODUCT, product})
+const createCartAction = product => ({type: CREATE_CART})
+const addToCartAction = product => ({type: ADD_TO_CART, product})
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchProducts = () => async dispatch => {
+  try {
+    const response = await axios.get('/api/products')
+    dispatch(fetchProductsAction(response.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const createCart = userId => async dispatch => {
+  try {
+    const response = await axios.post('/api/orders/')
+    dispatch(createCartAction())
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const addToCart = () => async dispatch => {
+  try {
+    const response = await axios.post('/api/orderstoitems/')
+    dispatch(fetchProductsAction(response.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const fetchProduct = id => async dispatch => {
+  try {
+    const response = await axios.get(`/api/products/${id}`)
+    dispatch(fetchProductAction(response.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = initialState, action) {
+  Object.freeze(state)
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+      return {...state, productList: action.products}
+    case FETCH_PRODUCT:
+      return {...state, productList: action.product}
+    default:
+      return state
+  }
+}
